dd2fb31d0f936f58756408de28dff4352fc820d5
Anton Yabchinskiy <arn@bestmx.ru>
2015-09-02 01:51:35 +0300

    Add resource_state() type

--- src/strava.erl
+++ src/strava.erl
@@ -1,7 +1,7 @@
 -module(strava).
 
 %% Types
--export_type([auth_token/0, position/0]).
+-export_type([auth_token/0, position/0, resource_state/0]).
 
 %%%===================================================================
 %%% Types
@@ -9,3 +9,4 @@
 
 -type auth_token() :: binary().
 -type position() :: {number(), number()}.
+-type resource_state() :: meta | summary | detailed.
