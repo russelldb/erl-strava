9328e9b0f2a885b67c6a5909c830ec31e7f06ab1
Anton Yabchinskiy <arn@bestmx.ru>
2015-09-01 12:27:10 +0300

    strava_upload: define functions

--- src/strava_upload.erl
+++ src/strava_upload.erl
@@ -24,13 +24,32 @@
 
 %%--------------------------------------------------------------------
 %% @doc
+%% Upload an activity.
 %% @end
 %%--------------------------------------------------------------------
 -spec file(strava:auth_token(), data_type(), file:filename_all()) -> status().
--spec file(strava:auth_token(), data_type(), file:filename_all(), map()) -> status().
+
+file(Token, DataType, FileName) ->
+    file(Token, DataType, FileName, _Options = #{}).
 
 %%--------------------------------------------------------------------
 %% @doc
+%% Upload an activity.
+%% @end
+%%--------------------------------------------------------------------
+-spec file(strava:auth_token(), data_type(), file:filename_all(), map()) -> status().
+
+file(_Token, _DataType, _FileName, _Options) ->
+    %% TODO
+    #{}.
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Check upload status.
 %% @end
 %%--------------------------------------------------------------------
 -spec status(strava:auth_token(), integer()) -> status().
+
+status(_Token, _Id) ->
+    %% TODO
+    #{}.
