8710405d779e2c2e5eba364710997007e5162cac
Anton Yabchinskiy <arn@bestmx.ru>
2015-09-03 22:05:51 +0300

    strava_auth: more auth related types

--- src/strava_auth.erl
+++ src/strava_auth.erl
@@ -1,10 +1,12 @@
 -module(strava_auth).
 
 %% Types
--export_type([token/0]).
+-export_type([approval_prompt/0, scope/0, token/0]).
 
 %%%===================================================================
 %%% Types
 %%%===================================================================
 
+-type approval_prompt() :: auto | force.
+-type scope() :: public | write | view_private | view_private_write.
 -type token() :: binary().
