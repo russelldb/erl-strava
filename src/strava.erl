ac42eb451c8cd86f477d459abe40fbab2f079acc
Anton Yabchinskiy <arn@bestmx.ru>
2015-09-05 03:21:15 +0300

    Add strava:start/0 function

--- src/strava.erl
+++ src/strava.erl
@@ -3,9 +3,31 @@
 %% Types
 -export_type([latlng/0, resource_state/0]).
 
+%% API
+-export([start/0]).
+
 %%%===================================================================
 %%% Types
 %%%===================================================================
 
 -type latlng() :: {number(), number()}.
 -type resource_state() :: meta | summary | detailed.
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc
+%% @end
+%%--------------------------------------------------------------------
+-spec start() -> ok.
+
+start() ->
+    case application:ensure_all_started(strava) of
+        {ok, _Apps} -> ok
+    end,
+    case inets:start(httpc, [{profile, strava}]) of
+        {ok, _Pid} -> ok;
+        {error, {already_started, _Pid}} -> ok
+    end.
