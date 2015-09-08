25a27b8e512580f7606b1945a79be4604ce0cbde
Anton Yabchinskiy <arn@bestmx.ru>
2015-09-08 17:51:01 +0300

    test/strava_repr_SUITE: remove init_per_suite, use load_data to load JSON and expected term

--- test/strava_repr_SUITE.erl
+++ test/strava_repr_SUITE.erl
@@ -2,29 +2,23 @@
 
 -include_lib("common_test/include/ct.hrl").
 
--export([all/0, init_per_suite/1]).
+-export([all/0]).
 
 -export([test_to_athlete/1]).
 
 all() ->
     [ test_to_athlete ].
 
-init_per_suite(Config) ->
-    PrivDir = ?config(data_dir, Config),
-    What = [ athlete ],
-    lists:foldl(
-      fun(Name, Ans) ->
-              NameBin = atom_to_binary(Name, latin1),
-              JSONPath = filename:join(PrivDir, <<NameBin/bytes, ".json">>),
-              TermPath = filename:join(PrivDir, <<NameBin/bytes, ".erl">>),
-              ct:pal("Loading ~s", [JSONPath]),
-              {ok, JSON} = file:read_file(JSONPath),
-              ct:pal("Loading ~s", [TermPath]),
-              {ok, [OutTerm]} = file:consult(TermPath),
-              InTerm = jsx:decode(JSON, [return_maps]),
-              [{Name, {InTerm, OutTerm}} | Ans]
-      end, Config, What).
-
 test_to_athlete(Config) ->
-    {In, Out} = ?config(athlete, Config),
-    Out = strava_repr:to_athlete(In).
+    {JSON, Term} = load_data(Config, <<"athlete">>),
+    Term = strava_repr:to_athlete(JSON).
+
+load_data(Config, Name) ->
+    DataDir = ?config(data_dir, Config),
+    JSONPath = filename:join(DataDir, <<Name/bytes, ".json">>),
+    TermPath = filename:join(DataDir, <<Name/bytes, ".erl">>),
+    ct:pal("Loading '~s' files:~n"
+           "~s~n~s", [Name, JSONPath, TermPath]),
+    {ok, JSON} = file:read_file(JSONPath),
+    {ok, [Term]} = file:consult(TermPath),
+    {jsx:decode(JSON, [return_maps]), Term}.
